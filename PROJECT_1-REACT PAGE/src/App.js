7b2e20c50ea3601d40a662b86012e7309f32bba9
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-08-05 14:08:00 +0530

    Add files via upload

---
+++ PROJECT_1-REACT PAGE/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./Hero";
+import Navigation from "./Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
